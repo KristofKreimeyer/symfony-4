7324be66f7d02863b8e1dd3ec6978078fc611022
kristof kreimeyer <kristof.kreimeyer@gmx.net>
2018-11-28 21:53:24 +0100

    fixtures multiple articles

---
+++ src/DataFixtures/BaseFixture.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Kristof
+ * Date: 28.11.2018
+ * Time: 21:34
+ */
+
+namespace App\DataFixtures;
+
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+abstract class BaseFixture extends Fixture
+{
+    /** @var ObjectManager */
+
+    private $manager;
+
+    public function load(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+        $this->loadData($manager);
+    }
+
+    abstract protected function loadData(ObjectManager $manager);
+
+
+    protected function createMany(string $className, int $count, callable $factory)
+
+    {
+        for ($i = 0; $i < $count; $i++) {
+            $entity = new $className();
+            $factory($entity, $i);
+            $this->manager->persist($entity);
+            // store for usage later as App\Entity\ClassName_#COUNT#
+            $this->addReference($className . '_' . $i, $entity);
+        }
+    }
+}

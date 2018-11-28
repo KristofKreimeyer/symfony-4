0b3f2b25f70def9f35ee840219182411390f4dbd
kristof kreimeyer <kristof.kreimeyer@gmx.net>
2018-11-28 19:04:54 +0100

    doctrine new fields

---
+++ src/Migrations/Version20181128173831.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181128173831 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE article ADD heart_count INT NOT NULL, ADD author VARCHAR(255) NOT NULL, ADD image_filename VARCHAR(255) DEFAULT NULL, CHANGE published_at published_at DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE article DROP heart_count, DROP author, DROP image_filename, CHANGE published_at published_at DATETIME DEFAULT \'NULL\'');
+    }
+}
